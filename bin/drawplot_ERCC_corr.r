f18b9b3ae0261a855581dbfddfeb0369b771be90
myoshimura080822 <myoshimura080822@gmail.com>
2020-12-01 17:08:04 +0900

    Fixed a bug that caused the correlation coefficient of ERCC to be calculated lower than actual.

--- bin/drawplot_ERCC_corr.r
+++ bin/drawplot_ERCC_corr.r
@@ -17,10 +17,30 @@ SDset <- function(x, y){
   data.frame(x=x, y=y, upper = mean(y)+3*sd(y),lower = mean(y)-3*sd(y))
 }
 
-ercc_countdata = read.table(erccfile, sep="\t", comment.char = "", header=T, stringsAsFactors=F)
-ercc_ref = read.table(erccref, sep="\t", comment.char = "", header=T, stringsAsFactors=F)
+calc_tpm <- function(counts,len) {
+  x <- counts/len
+    return(t(t(x)*1e6/colSums(x)))
+}
 
-plotdata = cor(ercc_countdata, ercc_ref$mollog)
+### load Ref
+ercc_ref = read.table(erccref, sep="\t", comment.char = "", header=T, stringsAsFactors=F)
+ercc_ref_trim = ercc_ref[,c("ERCC.ID","Length")]
+
+ercc_countdata = read.table(erccfile, sep="\t", comment.char = "", header=T, stringsAsFactors=F)
+ercc_countdata$ERCC.ID = rownames(ercc_countdata)
+ercc_countdata_tpm = dplyr::left_join(ercc_countdata, ercc_ref_trim, by=c("ERCC.ID"))
+rownames(ercc_countdata_tpm) = ercc_countdata_tpm$ERCC.ID
+
+# calc TPM
+ercc_countdata_tpm = calc_tpm(ercc_countdata_tpm[,!colnames(ercc_countdata_tpm) %in% c("ERCC.ID","Length"), drop=FALSE], as.numeric(ercc_countdata_tpm$Length))
+ercc_countdata_tpm = as.data.frame(ercc_countdata_tpm)
+ercc_countdata_tpm_log = log10(ercc_countdata_tpm+1)
+
+if (nrow(ercc_countdata_tpm_log) > 1){
+  write.table(ercc_countdata_tpm_log,"merged_featureCounts_gene_TPM_ERCC_log.txt",sep="\t", append=F, quote=F, row.names=T, col.names=T)
+}
+
+plotdata = cor(ercc_countdata_tpm_log, ercc_ref$mollog)
 plotdata = data.frame(corr=plotdata[,1],sample_name=as.character(rownames(plotdata)))
 plotdata = SDset(plotdata$sample_name, plotdata$corr)
 
